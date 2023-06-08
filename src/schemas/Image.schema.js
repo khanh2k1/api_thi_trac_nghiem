5a485ae97a8bbf5bfc2b981f26bd7bb9f5826f38
Dang Khanh <tanle6378@gmail.com>
2023-06-09 05:22:24 +0700

    update

--- src/schemas/Image.schema.js
+++ src/schemas/Image.schema.js
@@ -1,11 +1,10 @@
 // Định nghĩa schema Joi để xác thực tập tin hình ảnh
 const Joi = require("joi");
 const ImageSchema = Joi.object({
-  fieldname: Joi.string().required(),
-  buffer: Joi.binary().required(),
+  fieldname: Joi.string(),
+  buffer: Joi.binary(),
   mimeType: Joi.string()
-    .valid("image/jpeg", "image/png", "image/gif")
-    .required(),
+    .valid("image/jpeg", "image/png", "image/gif"),
 });
 
 module.exports = ImageSchema;
