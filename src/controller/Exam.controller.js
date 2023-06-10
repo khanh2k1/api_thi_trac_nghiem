c5b0c20f1233d02f20dd051b667c88ebc52e2fea
Dang Khanh <tanle6378@gmail.com>
2023-06-10 14:41:11 +0700

    update

--- src/controller/Exam.controller.js
+++ src/controller/Exam.controller.js
@@ -163,6 +163,7 @@ const ExamController = {
   update: async (req, res) => {
     const { _id } = req.params;
     const exam = await ExamModel.findOne({ _id });
+    console.log(req.body.questions)
     exam['questions'] = JSON.parse(req.body.questions)
     exam['correctAnswers'] = ExamUtils.parseStringToArray(req.body.correctAnswers)
     if (!exam) {
