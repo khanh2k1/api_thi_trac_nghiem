e7697971340ec9a2e8703bbe24e3775003f8dd05
Dang Khanh <tanle6378@gmail.com>
2023-06-12 02:08:09 +0700

    update

--- src/schemas/Result.schema.js
+++ src/schemas/Result.schema.js
@@ -9,6 +9,27 @@ const ResultSchema = {
     userAnswers: joi.array().items(
       joi.number().required()
     ).required(),
+
+    exam: joi.object(
+      {
+        examId: joi.string(),
+        name: joi.string().required().min(3),
+        totalTime: joi.number().required(),
+        description: joi.string().optional(),
+        image: joi.string(),
+
+        questions: joi.array().items(
+          joi.object({
+            questionText: joi.string().required(),
+            answers: joi.array().items(joi.string()).required()
+          })
+        ),
+
+        correctAnswers:  joi.array().items(joi.number()),
+
+        createdBy: joi.string().hex().length(24),
+        isPublic: joi.boolean().default(false),
+      })
   }),
 };
 
